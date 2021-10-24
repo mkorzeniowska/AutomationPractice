9c254d0a9646165a2b0009af04280a731f320b11
mkorzeniowska <karrenira@gmail.com>
2021-10-24 16:07:55 +0200

    Utworzenie base_element, base_page, start_page

---
+++ automation_practice/tests/pages/start_page.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+
+from .base_element import BaseElement
+from .base_page import BasePage
+from .locator import Locator
+
+class StartPage(BasePage):
+
+    url = 'http://automationpractice.com'
+
+    @property
+    def signature_text(self):
+        locator = Locator(by=By.XPATH, value='//div/h1')
+        return BaseElement(driver=self.driver,locator=locator)
+
+    @property
+    def search_input(self):
+        locator = Locator(by=By.ID, value='search_query_top')
+        return BaseElement(driver=self.driver,locator=locator)
+
+    @property
+    def search_button(self):
+        locator = Locator(by=By.NAME, locator='submit_search')
+        return BaseElement(driver=self.driver,locator=locator)
