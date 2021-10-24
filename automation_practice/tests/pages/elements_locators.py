9b3a344d57b0604d3749851283992a54d7a98153
mkorzeniowska <karrenira@gmail.com>
2021-10-25 00:55:22 +0200

    Utworzenie pliku z lokatorami

---
+++ automation_practice/tests/pages/elements_locators.py
@@ -0,0 +1,9 @@
+# Locators for all critical elements used in tests
+# Start page
+
+# Contact button
+contact_button_css = 'div#contact-link'
+# Sign in button
+sign_in_button_css = 'a[title="Log in to your customer account"]'
+# search input bar css id
+'#search_query_top'
