0862d6f047d1c1ee12bb1ca07912c01f5449cae5
mkorzeniowska <karrenira@gmail.com>
2021-10-25 20:25:14 +0200

    Zmiana nazwy start_page na home_page. Uzupełnienie lokatorów dla home page.

--- automation_practice/tests/test_home_page.py
+++ automation_practice/tests/test_home_page.py
@@ -1,9 +1,9 @@
-from pages.start_page import StartPage
+from pages.home_page import StartPage
 
 
-class StartPageTests:
+class HomePageTests:
 
-    def test_loading_start_page(self, browser):
+    def test_loading_home_page(self, browser):
         start_page = StartPage(driver=browser)
         start_page.go()
         assert start_page.signature_text.text == 'Automation Practice Website'
