b2a65d519e26158377acafdff617bc1ffcebcdfb
mkorzeniowska <karrenira@gmail.com>
2021-10-28 21:04:49 +0200

    refaktor nazw testów

--- automation_practice/tests/test_home_page.py
+++ automation_practice/tests/test_home_page.py
@@ -6,24 +6,24 @@ class HomePageTests:
         home_page = load_home_page
         assert home_page.title == 'My Store'
 
-    def test_contact_button_text_is_displayed(self, load_home_page):
+    def test_contact_button_is_displayed(self, load_home_page):
         home_page = load_home_page
         assert home_page.contact_button.text == 'Contact us'
 
-    def test_sign_in_button_text_is_displayed(self, load_home_page):
+    def test_sign_in_button_is_displayed(self, load_home_page):
         home_page = load_home_page
         assert home_page.sign_in_button.text == 'Sign in'
 
 
-    def test_menu_women_tab_text_is_displayed(self, load_home_page):
+    def test_menu_women_tab_is_displayed(self, load_home_page):
         home_page = load_home_page
         assert home_page.menu_women_tab.text == 'WOMEN'
 
-    def test_menu_dresses_text_tab_is_displayed(self, load_home_page):
+    def test_menu_dresses_tab_is_displayed(self, load_home_page):
         home_page = load_home_page
         assert home_page.menu_dresses_tab.text == 'DRESSES'
 
-    def test_menu_tshirts_tab_text_is_displayed(self, load_home_page):
+    def test_menu_tshirts_tab_is_displayed(self, load_home_page):
         home_page = load_home_page
         assert home_page.menu_tshirts_tab.text == 'T-SHIRTS'
 
